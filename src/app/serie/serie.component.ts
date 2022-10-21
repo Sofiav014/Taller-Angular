b8817f73d77c051eb391d3b569d5d64fdab4f3fa
Sofia Velasquez <s.velasquezm2@uniandes.edu.co>
2022-10-21 06:42:45 -0500

    .

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from './serie';
-import { dataSeries } from './dataSeries';
 import { SerieService } from './serie.service';
 
 @Component({
