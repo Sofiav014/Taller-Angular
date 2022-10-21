d71869d216896ad698b4adf0904bbd16f986b81b
Sofia Velasquez <s.velasquezm2@uniandes.edu.co>
2022-10-21 06:39:13 -0500

    Taller Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { SerieModule } from './serie/serie.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SerieModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/serie/serie.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Serie } from './serie';
+import { dataSeries } from './dataSeries';
+import { SerieService } from './serie.service';
+
+@Component({
+  selector: 'app-serie',
+  templateUrl: './serie.component.html',
+  styleUrls: ['./serie.component.css']
+})
+export class SerieComponent implements OnInit {
+
+  constructor(private serieService: SerieService) { }
+
+  series: Array<Serie> = [];
+  averageSeasons: number = 0;
+
+  getSeries() {
+    this.serieService.getSeries().subscribe(series => {
+      this.series = series;
+      let totalSeasons = 0;
+      series.forEach(serie => {
+        totalSeasons += serie.seasons;
+      });
+      this.averageSeasons = totalSeasons / series.length;
+    });
+
+  }
+
+  ngOnInit() {
+    this.getSeries();
+  }
+
+}
